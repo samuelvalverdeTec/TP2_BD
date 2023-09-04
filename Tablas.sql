5b451bd7302ba4d1847c8b40b84fc7f099be513d
samuelvalverdee <savalverde@estudiantec.cr>
2023-09-04 12:22:21 -0600

    tablas

---
+++ Tablas.sql
@@ -0,0 +1,44 @@
+USE [BD_TP2]
+GO
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+
+	CREATE TABLE dbo.Usuario
+	(
+	id INT IDENTITY (1, 1) NOT NULL PRIMARY KEY
+	, UserName VARCHAR(16) NOT NULL
+	, Password VARCHAR (16) NOT NULL
+	 );
+	-- ************************************************************
+	CREATE TABLE dbo.EventLog (
+	Id int IDENTITY(1,1) NOT NULL PRIMARY KEY
+	, LogDescription varchar(2000) NOT NULL
+	, PostIdUser int NOT NULL
+	, PostIP varchar(64) NOT NULL
+	, PostTime datetime NOT NULL
+	);
+
+	ALTER TABLE [dbo].[EventLog] WITH CHECK ADD CONSTRAINT
+	[FK_EventLog_Usuario] FOREIGN KEY([PostIdUser])
+	REFERENCES [dbo].[Usuario] ([id])
+	-- ************************************************************
+	CREATE TABLE dbo.ClaseArticulo
+	(
+	id INT IDENTITY (1, 1) PRIMARY KEY
+	, Nombre VARCHAR(64)
+	);
+	-- ************************************************************
+	CREATE TABLE dbo.Articulo
+	(
+	id INT IDENTITY (1, 1) PRIMARY KEY
+	, IdClaseArticulo INT
+	, Codigo VARCHAR(32) NOT NULL
+	, Nombre VARCHAR(128) NOT NULL
+	, Precio MONEY NOT NULL
+	);
+	
+	ALTER TABLE [dbo].[Articulo] WITH CHECK ADD CONSTRAINT
+	[FK_Articulo_ClaseArticulo] FOREIGN KEY([idClaseArticulo])
+	REFERENCES [dbo].[ClaseArticulo] ([id])
